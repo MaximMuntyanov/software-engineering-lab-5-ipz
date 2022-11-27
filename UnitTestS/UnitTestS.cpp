cc8667b3288731489a5c4a30c6a7f178e8bfa632
MaximMuntyanov <makczanta1996@gmail.com>
2022-11-27 13:46:22 +0200

    Init application

---
+++ UnitTestS/UnitTestS.cpp
@@ -0,0 +1,163 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\tboim\Desktop\Учеба\лаб ипз\lab3_ipz\lab3_ipz\lab3_ipz\lab3_ipz.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestS
+{
+	TEST_CLASS(calculate_Tests)
+	{
+	public:
+		TEST_METHOD(calculate_getnegative3and3_expected20)
+
+		{
+			double x = -3;
+			int n = 3;
+			double expected = 20;
+
+			double actual = calculate(x, n);
+
+			if (fabs(actual - expected) > 0.0001f)
+			{
+				Assert::IsTrue(true);
+			}
+			else
+			{
+				Assert::Fail();
+			}
+		}
+
+	public:
+		TEST_METHOD(calculate_get1and5_expected5251)
+		{
+			double x = 1;
+			int n = 3;
+			double expected = 5251;
+
+			double actual = calculate(x, n);
+
+			if (fabs(actual - expected) < 0.0001f)
+			{ 
+
+				Assert::IsTrue(true);
+			}
+			else
+			{
+				Assert::Fail();
+			}
+		}
+	};
+
+	TEST_CLASS(checkBounds_Tests)
+	{
+	public:
+		TEST_METHOD(checkBounds_get1and6_exceptionNotThrown)
+		{
+			int a = 1;
+			int b = 6;
+
+			try
+			{
+				checkBounds(a, b);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail;
+			}
+		} 
+
+
+	public:
+		TEST_METHOD(checkBounds_get14and3_exceptionThrown)
+		{
+			int a = 14;
+			int b = 3;
+
+			try
+			{
+				checkBounds(a, b);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+
+	TEST_CLASS(checkUpperBound_Tests)
+	{
+	public:
+		TEST_METHOD(checkUpperBound_get8_exceptionNotThrown)
+		{
+			int n = 8;
+
+			try 
+
+			{
+				checkUpperBound(n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+
+	public:
+		TEST_METHOD(checkUpperBound_get1_exceptionThrown)
+		{
+			int n = 1;
+
+			try
+			{
+				checkUpperBound(n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+
+	TEST_CLASS(checkStep_Tests)
+
+	{
+	public:
+		TEST_METHOD(checkStep_get1_exceptionNotThrown)
+		{
+			int h = 1;
+
+			try
+			{
+				checkStep(h);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+
+	public:
+		TEST_METHOD(checkStep_getnegative10_exceptionThrown)
+		{
+			int h = -10;
+
+			try
+			{
+				checkStep(h);
+				Assert::Fail();
+			} 
+
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+
+}
+
---
+++ lab3_ipz/lab3_ipz.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+
+#include <fstream>
+
+using namespace std;
+
+void checkInput()
+{
+    if (cin.fail())
+    {
+        throw "[Error] Incorrect input!";
+    }
+}
+
+void checkBounds(int a, int b)
+{
+    if (a >= b)
+    {
+        throw "[Error] Incorrect loop bounds input!";
+    }
+}
+
+void checkUpperBound(int n)
+{
+    if (n < 2)
+    {
+        throw "[Error] Incorrect upper bound input!";
+    }
+}
+
+void checkStep(int h)
+{
+    if (h <= 0)
+
+    {
+        throw "[Error] Incorrect step input!";
+    }
+}
+
+void filePrint(double x, double y, int n)
+{
+    ofstream fout("results.txt");
+    fout << "iteration number  " ", x = " << x << ", y = " << y << "\n";
+
+}
+
+
+double calculate(double x, int n)
+{
+    int y;
+	if (x < 0)  
+	{
+		int y = 1;
+		for (int j = 2; j <= n - 2; j++)
+		{
+
+			y *= j * j;
+
+		}
+		 
+	}
+
+	else if (x >= 0) 
+	{
+		int y = 0;
+		int p = 1;
+		for (int i = 0; i <= n - 1; i++)
+		{
+
+			for (int j = 0; j <= n - 1; j++)
+			{
+				y *= x + i * i + j;
+			}
+			y += p;
+
+		}
+        return y;
+	}
+   
+}
+
+
+int main()
+{
+    double a = 0;
+    double b = 0;
+    double h = 0;
+	int n = 0;
+
+    double y = 0;
+    double x = 0;
+
+    try
+    {
+        cout << "Please enter a, a < b ";
+        cin >> a;
+        cout << "Please enter b ";
+        cin >> b;
+        checkInput();
+        checkBounds(a, b);
+        cout << "Please enter h, h > 0 ";
+        cin >> h;
+        checkInput();
+        checkStep(h);
+        cout << "Please enter n, n > 2 ";
+        cin >> n;
+        checkInput();
+        checkUpperBound(n);
+
+        x = a;
+
+        while (x <= b)
+        {
+
+
+            y = calculate(x, n);
+            cout << "\'x\' and \'y\' values are: " << x << ", " << y << endl;
+
+            filePrint(x, y, n);
+
+            x += h;
+        }
+    }
+    catch (const char* ex)
+    {
+        cout << ex << endl;
+    }
+    catch (...)
+    {
+        cout << "[Error] Unknown error" << endl;
+        return -2;
+    }
+
+    return 0;
+
+}
